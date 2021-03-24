04fc06f0407ef383801064429afefa6e7afed25a
Maxim Kazachuk <dem2bb1@gmail.com>
2021-03-24 17:37:19 +0200

    added best routes you ever seen in your life

--- src/views/products/Products.jsx
+++ src/views/products/Products.jsx
@@ -1,17 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { productRoutes } from '../../routes/productRoutes';
-import NavigationList from '../../components/navigation/NavigationList';
-import NavigationRoutes from '../../components/navigation/NavigationRoutes';
-import { useRouteMatch } from 'react-router-dom';
+import InnerNavigation from '../../components/innerNavigation/InnerNavigation';
 
 const Products = () => {
-  const match = useRouteMatch('');
-  return (
-    <>
-      <NavigationList routes={productRoutes} match={match.url} />
-      <NavigationRoutes routes={productRoutes} match={match.url} />
-    </>
-  );
+  return <InnerNavigation routes={productRoutes} />;
 };
 
 export default Products;
