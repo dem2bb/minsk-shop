00ed36cc8980afb9784b6adbd0269f918ef111f3
Maxim Kazachuk <dem2bb1@gmail.com>
2021-03-25 16:34:36 +0200

    25

--- src/components/modal/Modal.hoc.js
+++ src/components/modal/Modal.hoc.js
@@ -1,55 +1,99 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { CSSTransition } from 'react-transition-group';
 import sprite from '../../icons/symbol-defs.svg';
 import Overlay from './ModalStyled';
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleEscape);
-  }
+const Modal = ({ children, onClose, isOpen }) => {
+  useEffect(() => {
+    window.addEventListener('keydown', handleEscape);
+    return () => {
+      window.removeEventListener('keydown', handleEscape);
+    };
+  }, []);
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleEscape);
-  }
-
-  handleEscape = event => {
+  const handleEscape = event => {
     if (event.code === 'Escape') {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  handleBackdropClick = event => {
+  const handleBackdropClick = event => {
     if (event.target.dataset?.zone) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    return (
-      <Overlay onClick={this.handleBackdropClick} data-zone="overlay">
-        <CSSTransition
-          in={this.props.isOpen}
-          appear={true}
-          timeout={300}
-          classNames="modal"
-          unmountOnExit
-        >
-          <div className="Modal">
-            {this.props.children}
-            <button
-              className="modalBtn"
-              onClick={this.props.onClose}
-              type="button"
-            >
-              <svg className="modalIcon" width="24px" height="24px">
-                <use href={sprite + '#icon-cross'} />
-              </svg>
-            </button>
-          </div>
-        </CSSTransition>
-      </Overlay>
-    );
-  }
-}
+  return (
+    <Overlay onClick={handleBackdropClick} data-zone="overlay">
+      <CSSTransition
+        in={isOpen}
+        appear={true}
+        timeout={300}
+        classNames="modal"
+        unmountOnExit
+      >
+        <div className="Modal">
+          {children}
+          <button className="modalBtn" onClick={onClose} type="button">
+            <svg className="modalIcon" width="24px" height="24px">
+              <use href={sprite + '#icon-cross'} />
+            </svg>
+          </button>
+        </div>
+      </CSSTransition>
+    </Overlay>
+  );
+};
 
 export default Modal;
+
+// class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleEscape);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleEscape);
+//   }
+
+//   handleEscape = event => {
+//     if (event.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+
+//   handleBackdropClick = event => {
+//     if (event.target.dataset?.zone) {
+//       this.props.onClose();
+//     }
+//   };
+
+//   render() {
+//     return (
+//       <Overlay onClick={this.handleBackdropClick} data-zone="overlay">
+//         <CSSTransition
+//           in={this.props.isOpen}
+//           appear={true}
+//           timeout={300}
+//           classNames="modal"
+//           unmountOnExit
+//         >
+//           <div className="Modal">
+//             {this.props.children}
+//             <button
+//               className="modalBtn"
+//               onClick={this.props.onClose}
+//               type="button"
+//             >
+//               <svg className="modalIcon" width="24px" height="24px">
+//                 <use href={sprite + '#icon-cross'} />
+//               </svg>
+//             </button>
+//           </div>
+//         </CSSTransition>
+//       </Overlay>
+//     );
+//   }
+// }
+
+// export default Modal;
